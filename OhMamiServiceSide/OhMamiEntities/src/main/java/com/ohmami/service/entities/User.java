1338ba0bf50b7066af5c36e89b46ed9021cf5a77
Christos <christos.karalis@gmail.com>
2012-12-06 13:09:54 +0100

    driver passenger

--- OhMamiServiceSide/OhMamiEntities/src/main/java/com/ohmami/service/entities/User.java
+++ OhMamiServiceSide/OhMamiEntities/src/main/java/com/ohmami/service/entities/User.java
@@ -1,10 +1,27 @@
 package com.ohmami.service.entities;
 
 public class User {
-
 	
 	private String firstName;
 	private String surname;
 	private String userName;
-
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 }
---
+++ OhMamiServiceSide/OhMamiServiceInterfaces/src/main/java/com/ohmami/service/interfaces/AuthenticateService.java
@@ -0,0 +1,9 @@
+package com.ohmami.service.interfaces;
+
+import com.ohmami.service.entities.User;
+
+public interface AuthenticateService {
+
+	void loginUser(User user);
+	
+}
